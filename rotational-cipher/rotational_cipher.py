a9c0687966b7b496a97a95b5c68990cb3c016538
AindriyaBarua <barua.aindriya@gmail.com>
2017-09-21 03:56:10 +0000

    Update rotational_cipher.py

--- rotational-cipher/rotational_cipher.py
+++ rotational-cipher/rotational_cipher.py
@@ -1,12 +1,16 @@
 def rotate(text,key):
+    new=''
     for i in range(0,len(text)):
         ch=text[i]
         if ch.isalpha():
             change=(ord(ch)+key)
-            if (change <=90 and ch.isupper()) or (change<=122 and ch.islower()):
-                text=text.replace(text[i],str(change))
-            elif ch.islower():
-                text=text.replace(text[i],str(65+change-90))
-            elif ch.islower():
-                text=text.replace(text[i],str(97+change-122))
-    return text
+            if ch.isupper():
+                if change >90:
+                    change= 66+change-90-2
+            elif (ch.islower()):
+                if change>122:
+                    change= 96+change-122
+            new=new+chr(change)
+        else:
+            new=new+ch
+    return new
